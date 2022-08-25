b702da12e5481ca8512fa4aff6652171dfd7cfc5
ccarlson41 <ccarlson41l@huskers.unl.edu>
2022-08-25 13:32:08 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Cam carlson
+ *         ccarlson41@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Cameron Carlson\n");
+
+  return 0;
+}
